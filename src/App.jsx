29e00bcc6abf015045ff07224d5b91dd7fe24454
dachishengelia <dachishengelia9@gmail.com>
2024-11-02 16:47:34 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <form className='form'>
+      <div>
+       <h1 className='text'>gamarjoba mas</h1>
+      </div>
+      <div className='artiomi'>
+       <h1>name: tualetis qagaldi</h1>
+       <h1>price: 99$</h1>
+       <h1>desc:tualetshi</h1>
+       <h1>color: white</h1>
+      </div>
+      <div><button className='button'>ragacistvis</button></div>
+      </form>
+    </>
+  )
+}
+
+export default App
